fa1b16ba96fdfc36c0be7b3b6b7d7c5c7f2f2bd1
Maxi Merrillees <maxi.merrillees@gmail.com>
2019-02-12 19:42:49 +1100

    Add redirect functionality on edit

--- src/assets/js/components/EditListing.js
+++ src/assets/js/components/EditListing.js
@@ -1,43 +1,75 @@
 import React from 'react'
+import { Redirect } from 'react-router'
 import { data } from '../dummyData'
 
-const EditListing = (props) => {
-  let property
-  data.forEach((current, index, array) => {
-    if(current.id === parseInt(props.match.params.id)) {
-      property = current
-    }
-  })
-
-  const handleOnSubmit = (event) => {
-    event.preventDefault()
-
-    property.address = event.target.address.value
-    property.city = event.target.city.value
-    property.description = event.target.description.value
-    property.bedrooms = event.target.bedrooms.value
-    property.bathrooms = event.target.bathrooms.value
-    property.garages = event.target.garages.value
-    property.price = event.target.price.value
+class EditListing extends React.Component {
+  state = {
+    property: {},
+    edited: false,
   }
 
-  return (
-    <div>
-      <img src={property.mainImage} alt=""/>
-      <form onSubmit={handleOnSubmit}>
-        <input type="text" name="address" defaultValue={property.address} />
-        <input type="text" name="city" defaultValue={property.city} />
-        <input type="text" name="description" defaultValue={property.description} />
-        <input type="text" name="bedrooms" defaultValue={property.bedrooms} />
-        <input type="text" name="bathrooms" defaultValue={property.bathrooms} />
-        <input type="text" name="garages" defaultValue={property.garages} />
-        <input type="text" name="price" defaultValue={property.price} />
-        <button>Save changes</button>
-      </form>
-      
-      <h3>{property.address}, {property.city}</h3>
-    </div>
-  )
+  handleOnSubmit = (event) => {
+    event.preventDefault()
+    console.log(this.state.property)
+
+    data.forEach((current, index, array) => {
+      if(current.id === parseInt(this.state.property.id)) {
+        current.address = event.target.address.value
+        current.city = event.target.city.value
+        current.description = event.target.description.value
+        current.bedrooms = event.target.bedrooms.value
+        current.bathrooms = event.target.bathrooms.value
+        current.garages = event.target.garages.value
+        current.price = event.target.price.value
+      }
+    })
+
+    this.setState(() => {
+      return {
+        edited: true
+      }
+    })
+
+    console.log(data)
+  }
+
+  componentDidMount() {
+    let property
+    data.forEach((current, index, array) => {
+      if(current.id === parseInt(this.props.match.params.id)) {
+        property = current
+      }
+    })
+    this.setState(() => {
+      return {
+        property: property
+      }
+    })
+  }
+
+  render() {
+    if(!this.state.edited) {
+      return (
+        <div>
+          <img src={this.state.property.mainImage} alt=""/>
+          <form onSubmit={this.handleOnSubmit}>
+            <input type="text" name="address" defaultValue={this.state.property.address} />
+            <input type="text" name="city" defaultValue={this.state.property.city} />
+            <input type="text" name="description" defaultValue={this.state.property.description} />
+            <input type="text" name="bedrooms" defaultValue={this.state.property.bedrooms} />
+            <input type="text" name="bathrooms" defaultValue={this.state.property.bathrooms} />
+            <input type="text" name="garages" defaultValue={this.state.property.garages} />
+            <input type="text" name="price" defaultValue={this.state.property.price} />
+            <button>Save changes</button>
+          </form>
+        </div>
+      )
+    } else {
+      return <Redirect to={`/listing/${this.state.property.id}`} />
+    }
+    
+  }
+  
 }
 
 export { EditListing }
