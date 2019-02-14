1a5a21a1ccea0a5f401890c76dada36d6ec64ade
Maxi Merrillees <maxi.merrillees@gmail.com>
2019-02-14 14:27:08 +1100

    Add footer

--- src/assets/js/components/Footer.js
+++ src/assets/js/components/Footer.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
-
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="component-footer">
+      <p>&copy; Cari Rumah Real Estate</p>
     </div>
   )
 }
