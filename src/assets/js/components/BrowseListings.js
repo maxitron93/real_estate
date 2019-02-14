601e6ef8dde8e9d5f156e773d53b9d2b93e5db27
Maxi Merrillees <maxi.merrillees@gmail.com>
2019-02-14 22:48:51 +1100

    Add search functionality

--- src/assets/js/components/BrowseListings.js
+++ src/assets/js/components/BrowseListings.js
@@ -3,21 +3,85 @@ import queryString from 'query-string'
 import { data } from '../dummyData'
 import { ListingInfo } from './ListingInfo'
 
-const BrowseListings = (props) => {
-  let queryStringObject = queryString.parse(props.location.search)
-  console.log(queryStringObject.keywords)
-  console.log(queryStringObject.city)
-  console.log(queryStringObject.bedrooms)
-  console.log(queryStringObject.maxPrice)
+class BrowseListings extends React.Component {
+  state = {
+    keywords: queryString.parse(this.props.location.search).keywords,
+    city: queryString.parse(this.props.location.search).city,
+    maxBedrooms: queryString.parse(this.props.location.search).bedrooms,
+    maxPrice: queryString.parse(this.props.location.search).maxPrice,
+    properties: []
+  }
 
-  return (
-    <div className="component-browseListings">
-      <h2>BrowseListings</h2>
-      {data.map((current, index, array) => {
-        return <ListingInfo key={index} property={current}/>
-      })}
-    </div>
-  )
+  componentDidMount() {
+    let properties = data.filter((current, index, array) => {
+      let keywordsMatch = true
+      let cityMatch = true
+      let bedroomsMatch = true
+      let maxPriceMatch = true
+
+      if(this.state.keywords) {
+        if(current.description.includes(this.state.keywords)) {
+          keywordsMatch = true
+        } else {
+          keywordsMatch = false
+        }
+      }
+
+      if(this.state.city) {
+        if(this.state.city === current.city) {
+          cityMatch = true
+        } else {
+          cityMatch = false
+        }
+      }
+
+      if(this.state.maxBedrooms) {
+        if(this.state.maxBedrooms >= parseInt(current.bedrooms)) {
+          bedroomsMatch = true
+        } else {
+          bedroomsMatch = false
+        }
+      }
+
+      if(this.state.maxPrice) {
+        if(this.state.maxPrice >= parseInt(current.price)) {
+          maxPriceMatch = true
+        } else {
+          maxPriceMatch = false
+        }
+      }
+
+      if(keywordsMatch && cityMatch && bedroomsMatch && maxPriceMatch) {
+        return current
+      }
+    })
+
+    this.setState(() => {
+      return {
+        properties: properties
+      }
+    })
+  }
+
+  render() {
+    if(this.state.properties.length !== 0) {
+      return (
+        <div className="component-browseListings">
+          <h2>BrowseListings</h2>
+          {this.state.properties.map((current, index, array) => {
+            return <ListingInfo key={index} property={current}/>
+          })}
+        </div>
+      )
+    } else {
+      return (
+        <div className="component-browseListings">
+          <h2>No Listings Found</h2>
+        </div >
+      )
+    }
+  }
+  
 }
 
 export { BrowseListings }
